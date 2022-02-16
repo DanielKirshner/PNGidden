5583f28f42010f41c6f1b1c86f07970282f30a3a
DanielKirshner <danielkirshner7878@gmail.com>
2022-02-16 22:45:20 +0200

    🔐 Encoding the message with the LSB's in the PNG

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import PIL.Image
-import os    
+import os
 
 
 def is_file_exists(file_path):
@@ -50,26 +50,46 @@ def extract_exe_from_image(image_path):
     return f"Successfully extracted exe file from the image!\nNew exe is -> {exe_path}"
 
 
-def main():
-    message_to_hide = "This is my secret!"
-    image = PIL.Image.open('image.png', 'r')
+def hide_message_in_image(image_path, message_to_hide):
+    if is_file_exists(image_path) == False:
+        return "Image path is invalid."
+    if is_file_png(image_path) == False:
+        return "The image is not a PNG file."
+
+    image = PIL.Image.open(image_path, 'r')
     width, height = image.size
     img_arr = np.array(list(image.getdata()))
 
     if image.mode == 'P':
-        print("Not Supported.")
-        exit()
+        return "Image not supported."
     
     channels = 4 if image.mode == 'RGBA' else 3
     pixels = img_arr.size // channels
     stop_indicator = "$STOP$"
-    stop_indicator_length = len(stop_indicator)
-
     message_to_hide += stop_indicator
-
     byte_message = ''.join(f"{ord(c):08b}" for c in message_to_hide)
-    print(byte_message)
+    print(f"Message to hide (in bits) :\n{byte_message}")
+    bits = len(byte_message)
 
+    if bits > pixels:
+        return "Not enough space to encode the message"
+    else:
+        index = 0
+        for i in range(pixels):
+            for j in range(0, 3):
+                if index < bits:
+                    img_arr[i][j] = int(bin(img_arr[i][j])[2:-1] + byte_message[index], 2)
+                    index += 1
+    img_arr = img_arr.reshape((height, width, channels))
+    result = PIL.Image.fromarray(img_arr.astype('uint8'), image.mode)
+    encoded_image_path = 'encoded.png'
+    result.save(encoded_image_path)
+    return f"Successfully hidden the message inside the image!\nNew png file is -> {encoded_image_path}"
+
+
+def main():
+    hiding_result = hide_message_in_image('image.png', 'This is my secret!')
+    print(hiding_result)
 
 
 if __name__ == '__main__':
